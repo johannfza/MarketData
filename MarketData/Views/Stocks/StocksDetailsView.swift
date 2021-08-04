fe82d005dd1135e3255ac9e1cd189b2e73615a2c
Johann Fong <johannfza@gmail.com>
2021-08-05 00:36:50 +0800

    feat: add StocksDetailsView

---
+++ MarketData/Views/Stocks/StocksDetailsView.swift
@@ -0,0 +1,23 @@
+//
+//  StocksDetailsView.swift
+//  MarketData
+//
+//  Created by Johann Fong  on 5/8/21.
+//
+
+import SwiftUI
+
+struct StocksDetailsView: View {
+    
+    var symbol: String
+    
+    var body: some View {
+        Text(symbol)
+    }
+}
+
+struct StocksDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        StocksDetailsView(symbol: "TSLA")
+    }
+}
