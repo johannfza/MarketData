a4c88cea1895458277f0388efb73cebb5d5be963
Johann Fong <johannfza@gmail.com>
2021-08-05 00:02:12 +0800

    feat: created RoundedRectangleImageView

--- MarketData/Views/Common/RoundedRectangleImageView.swift
+++ MarketData/Views/Common/RoundedRectangleImageView.swift
@@ -6,18 +6,36 @@
 //
 
 import SwiftUI
+import SDWebImageSwiftUI
 
 struct RoundedRectangleImageView: View {
     
     var urlString: String = ""
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        WebImage(url: URL(string: urlString))
+            .resizable()
+            .placeholder {
+                Rectangle().cornerRadius(8).foregroundColor(Color(UIColor.lightGray))
+            }
+            .indicator(.activity)
+            .frame(width: 80, height: 80)
+            .clipShape(RoundedRectangle(cornerSize: CGSize(width: 8.0, height: 8.0)))
+            .shadow(radius: 5)
+            .padding(3)
     }
 }
 
 struct RoundedRectangleImageView_Previews: PreviewProvider {
     static var previews: some View {
-        RoundedRectangleImageView()
+        Group {
+            RoundedRectangleImageView()
+        }
+        .previewLayout(.fixed(width: 100, height: 100))
+        
+        Group {
+            RoundedRectangleImageView(urlString: "https://nokiatech.github.io/heif/content/images/ski_jump_1440x960.heic")
+        }
+        .previewLayout(.fixed(width: 100, height: 100))
     }
 }
