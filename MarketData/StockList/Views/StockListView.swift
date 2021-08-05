5eea2a67b618242d36d74e1e55b680506f71336b
Johann Fong <johannfza@gmail.com>
2021-08-05 14:44:31 +0800

    fix: StockRowView typo

--- MarketData/StockList/Views/StockListView.swift
+++ MarketData/StockList/Views/StockListView.swift
@@ -21,21 +21,7 @@ struct StockListView: View {
 }
 
 struct StockList_Previews: PreviewProvider {
-    
-    
-    static var stocks: [StockInfoModel] {
-        var stocks = [StockInfoModel]()
-        for i in 0...5 {
-            let tradePrice = Amount(value: 650.00, currency: .usd)
-            let actualChange = Amount(value: 0.00, currency: .usd)
-            let price = PriceInfo(symbol: "TSLA", tradePrice: tradePrice, actualChange: actualChange, percentageChange: 10.00)
-            let stock = StockInfoModel(name: "TESLA", symbol: "TSLA\(i)", imageURL: nil, priceInfo: price)
-            stocks.append(stock)
-        }
-        return stocks
-    }
-    
     static var previews: some View {
-        StockListView(stocks: stocks)
+        StockListView(stocks: StockListViewModel().stocks)
     }
 }
--- MarketData/StockList/Views/StockRowView.swift
+++ MarketData/StockList/Views/StockRowView.swift
@@ -11,15 +11,16 @@ struct StockRowView: View {
     
     public var stock: StockInfoModel
     
-    
     var body: some View {
         HStack {
+            RoundedRectangleImageView(urlString: stock.imageURL ?? "")
             VStack(alignment: .leading) {
                 Text(stock.name)
                     .font(.headline)
                 Text(stock.symbol)
                     .font(.subheadline)
             }
+            .padding(.leading, 5)
             Spacer()
             VStack(alignment: .trailing) {
                 Text(((stock.priceInfo.tradePrice.getValue()) as String))
@@ -35,14 +36,10 @@ struct StockRowView: View {
 
 struct StockRow_Previews: PreviewProvider {
     
-    static let tradePrice = Amount(value: 650.00, currency: .usd)
-    static let actualChange = Amount(value: 0.00, currency: .usd)
-    static let price = PriceInfo(symbol: "TSLA", tradePrice: tradePrice, actualChange: actualChange, percentageChange: 10.00)
-    static let stock = StockInfoModel(name: "TESLA", symbol: "TSLA", imageURL: nil, priceInfo: price)
-    
     static var previews: some View {
-        List {
-            StockRowView(stock: stock)
+        Group {
+            StockRowView(stock: StockListViewModel().stocks[0])
         }
+        .previewLayout(.fixed(width: 450, height: 100))
     }
 }
